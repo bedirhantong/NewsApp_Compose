c757e1be8a0c430542cd57219f1b90ae8891e142
Bedirhan Tonğ <20200808033@ogr.akdeniz.edu.tr>
2024-05-21 18:53:38 +0300

    implementation base is done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -30,17 +32,22 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
+    }
+    kotlin{
+        jvmToolchain {
+            languageVersion.set(JavaLanguageVersion.of(17))
+        }
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.4.3"
     }
     packaging {
         resources {
@@ -52,19 +59,6 @@ android {
 dependencies {
 
 
-    implementation ("com.squareup.okhttp3:logging-interceptor:4.7.2")
-
-    // for using placeholder while actual image is loading
-    implementation ("com.github.skydoves:landscapist-coil:1.4.1")
-
-    //Retrofit https://square.github.io/retrofit/
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-
-    // converter for class com.example.news_compose.models.TopNewsResponse
-    implementation ("com.squareup.moshi:moshi-kotlin:1.9.3")
-    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
-
-    implementation("androidx.compose.material:material")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
@@ -82,4 +76,40 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.navigation:navigation-compose:2.5.0-rc01")
+
+
+
+
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.7.2")
+
+    // for using placeholder while actual image is loading
+    implementation ("com.github.skydoves:landscapist-coil:1.4.1")
+
+    //Retrofit https://square.github.io/retrofit/
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
+    // converter for class com.example.news_compose.models.TopNewsResponse
+    implementation ("com.squareup.moshi:moshi-kotlin:1.9.3")
+    implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
+
+    implementation("androidx.compose.material:material")
+
+
+    // Room DB
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    androidTestImplementation("androidx.room:room-testing:2.6.1")
+
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+
+    // Hilt Dagger
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    kapt("androidx.hilt:hilt-compiler:1.2.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
 }
